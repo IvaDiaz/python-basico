9519a851534b3afb516a7bfc3aa66ffac06ea5bc
Ivan <shadow.ivano@gmail.com>
2026-01-03 14:05:58 -0500

    Programa para clasificar numeros

---
+++ main.py
@@ -0,0 +1,13 @@
+numero = int(input("Ingresa un numero: "))
+
+if numero > 0:
+    print ("el numero es positivo")
+
+elif numero < 0:
+    print ("el numero es negativo")
+
+else:
+    print ("es cero")
+
+print ("Gracias por enseñarme chatgpt")
+
